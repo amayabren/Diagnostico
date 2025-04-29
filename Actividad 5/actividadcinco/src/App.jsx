bfd21120dae675c7f64b5708e6d27759878461b9
Brenda Amaya <brendaamaya9@gmail.com>
2025-04-28 22:54:03 -0300

    act 5

---
+++ Actividad 5/actividadcinco/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+        Hacer clic
+      </button>
+    </div>
+  );
+}
+
+export default App;
